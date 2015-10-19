8ea0fd5da7c323dca805eba2e10bccec94daa353
Lucas Lindström <ztargaser@gmail.com>
2015-10-19 19:50:18 +0200

    Missed a bit of coverage.

--- test/utility/defaultloggertest.cpp
+++ test/utility/defaultloggertest.cpp
@@ -29,7 +29,7 @@ protected:
     const std::stringstream stdoutStream;
 
     DefaultLoggerTest()
-        : logger(unit, std::cout)
+        : logger(unit)
     {
         // Capture stdout
         stdoutBuffer = std::cout.rdbuf();
