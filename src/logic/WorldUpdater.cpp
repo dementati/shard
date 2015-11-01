c25975199c557b9c378d812132755a489336825c
Lucas Lindström <ztargaser@gmail.com>
2015-11-01 22:51:16 +0100

    Added a system for adding background jobs to objects.

--- src/logic/WorldUpdater.cpp
+++ src/logic/WorldUpdater.cpp
@@ -8,8 +8,27 @@ WorldUpdater::WorldUpdater(World &world)
 
 void WorldUpdater::update(unsigned int dt)
 {
-    for(std::unique_ptr<Entity>& entity : mWorld.getEntities())
+    for(auto &object : mWorld.getObjects())
     {
+        if(object->hasAttribute("backgroundJobs"))
+        {
+            for(auto &job : (*object)["backgroundJobs"].get<std::vector<std::shared_ptr<Job>>>())
+            {
+                job->execute(dt);
+            }
+        }
+    }
+
+    for(auto &entity : mWorld.getEntities())
+    {
+        if(entity->hasAttribute("backgroundJobs"))
+        {
+            for(auto &job : (*entity)["backgroundJobs"].get<std::vector<std::shared_ptr<Job>>>())
+            {
+                job->execute(dt);
+            }
+        } 
+
         Need& need = entity->selectNeed();
         need.execute(dt);
     }
